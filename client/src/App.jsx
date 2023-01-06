11ec9dcb545aa57b210f84bef9975944601efd17
Arup Mandal <40239193+arupmandal@users.noreply.github.com>
2023-01-06 19:51:08 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,8 +17,8 @@ function App() {
 
     const fetchBotResponse = async () => {
         const { data } = await axios.post(
-            "https://chatgpt-ai-app-od21.onrender.com",
-            // "http://localhost:5173",
+            "https://arup1-bot.onrender.com",
+            
             { input },
             {
                 headers: {
